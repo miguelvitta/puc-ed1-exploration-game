6c61d0a90b6b775612d394bbfd53b2ca197afbbe
miguelvitta <miguelvittadev@gmail.com>
2025-11-30 10:44:59 -0300

    feat: implements the cell types

--- src/map/Cell.c
+++ src/map/Cell.c
@@ -6,3 +6,33 @@ Implement cell type utilities:
 - Identify cell categories (wall, free, chest, door, explorer, exit)
 - Helper functions for checking type
 */
+
+int cell_is_walkable(CellType type) {
+    return (type == CELL_FREE || type == CELL_EXIT);
+}
+
+CellType cell_from_char(char c) {
+    switch (c)
+    {
+        case ' ': return CELL_FREE;
+        case '*': return CELL_WALL;
+        case '$': return CELL_DOOR;
+        case 'B': return CELL_CHEST;
+        case '@': return CELL_EXIT;
+        case '#': return CELL_FREE;
+        default: return CELL_FREE;
+    }
+}
+
+char cell_to_char(CellType type) {
+    switch (type) {
+        case CELL_FREE: return ' ';
+        case CELL_WALL: return '*';
+        case CELL_DOOR: return '$';
+        case CELL_CHEST: return 'B';
+        case CELL_EXIT: return '@';
+        case CELL_EXPLORER: return '#';
+        default: return '?';
+    }
+}
+
--- src/map/Cell.h
+++ src/map/Cell.h
@@ -3,4 +3,25 @@
 
 // Represent a single cell of the map using enum or char and categorize it (wall, chest, door, free space, explorer, exit).
 
+typedef enum {
+    CELL_FREE,
+    CELL_WALL,
+    CELL_DOOR,
+    CELL_CHEST,
+    CELL_EXIT,
+    CELL_EXPLORER
+} CellType;
+
+typedef struct {
+    CellType type;
+} Cell;
+
+int cell_is_walkable(CellType type);
+
+// converts a raw character from the map to our enum
+CellType cell_from_char(char c);
+
+// Converts the Enum back to a character
+char cell_to_char(CellType type);
+
 #endif
